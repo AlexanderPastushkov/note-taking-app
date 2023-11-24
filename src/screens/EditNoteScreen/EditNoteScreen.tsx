b8b3cf357239eaec278182506ecbaee243160f88
Alexander <alexanderpastfast@gmail.com>
2023-11-24 12:58:07 +0300

    feat:add ability to generate id's

--- src/screens/EditNoteScreen/EditNoteScreen.tsx
+++ src/screens/EditNoteScreen/EditNoteScreen.tsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
 import { NoteTakingInput } from "../Note/NoteTakingInput";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { saveNote } from "../../services/noteStoreService";
 
 type Props = {};
 
 export const EditNoteScreen = (props: Props) => {
-  const saveNote = async (text: string) => {
-    try {
-      await AsyncStorage.setItem("note", text);
-    } catch (e) {
-      // saving error
-    }
-  };
   return <NoteTakingInput saveNote={saveNote} />;
 };
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -2,25 +2,23 @@ import { StyleSheet, Text, Button, View } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { ScreenNavigationProp } from "../../types/types";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Note, getAllNotes, getNote } from "../../services/noteStoreService";
 
 type Props = {};
 
 export const HomeScreen = (props: Props) => {
   const navigation = useNavigation<ScreenNavigationProp>();
-  const [noteText, setNoteText] = useState<string>("");
+  const [notes, setNotes] = useState<Note[]>([]);
   useFocusEffect(() => {
-    getNote().then((result) => setNoteText(result));
+    getAllNotes().then((result) => setNotes(result.notes));
   });
-
-  const getNote = async () => {
-    return AsyncStorage.getItem("note");
-  };
 
   return (
     <>
       <View>
-        <Text>{noteText}</Text>
+        {notes.map((note) => (
+          <Text key={note.id}>{note.text}</Text>
+        ))}
       </View>
       <Button
         title="New note"
